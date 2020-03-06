60abc71ba405a82534a4b6a10dae7415b94419f7
Liam Crowley <liamdcrowley@gmail.com>
2020-03-06 14:25:16 +1300

    update

--- WinPython-PyGaze-0.6.0/WinPython-PyGaze-0.6.0/python-2.7.3/Scripts/frl_reading/FRL_reading.py
+++ WinPython-PyGaze-0.6.0/WinPython-PyGaze-0.6.0/python-2.7.3/Scripts/frl_reading/FRL_reading.py
@@ -1,4 +1,5 @@
 import random
+import numpy
 from constants import *
 from pygaze import libtime
 from pygaze.libscreen import Display, Screen
@@ -26,12 +27,14 @@ log.write(["gazepos", "trialnr", "trialstart", "trialend", "duration", "image"])
 # run trials
 tracker.calibrate()
 
-# set trial type
-trialtype = random.choice(['mask','aperture'])
-
 # run experiment
-if (trialtype == 'mask'):
-	for trialnr in range(0,len(IMAGES)):
+for trialnr in range(0,1):
+	#trialType = random.choice(['mask', 'window', 'full'])
+	trialType = 'mask'
+	maskCount = 0
+	windowCount = 0
+	fullCount = 0
+	if (trialType == 'window' and maskCount < 3):
 		# blank display
 		disp.fill()
 		disp.show()
@@ -39,6 +42,9 @@ if (trialtype == 'mask'):
 		# prepare stimulus
 		scr.clear()
 		scr.draw_image(IMAGES[trialnr])
+		disp.fill(screen=scr)
+		disp.show()
+		libtime.pause(1000)
 		# start recording eye movements
 		tracker.drift_correction()
 		tracker.start_recording()
@@ -52,18 +58,16 @@ if (trialtype == 'mask'):
 			gazepos = tracker.sample()
 			frl.update(disp, scr, gazepos)
 			response, presstime = kb.get_key(timeout=1)
-
 		scr.draw_image(IMAGES[trialnr])
 		frl = FRL(pos='center', dist=125, size=200000)
 		frl.update(disp, scr, gazepos)
 		libtime.pause(1000)
-
+		windowCount = windowCount + 1
 		# stop tracking and process input
 		tracker.stop_recording()
 		tracker.log("stop trial %d" % trialnr)
 		log.write([gazepos, trialnr, trialstart, presstime, presstime - trialstart, IMAGES[trialnr]])
-else:
-	for trialnr in range(0,len(IMAGES)):
+	elif (trialType == 'mask' and windowCount < 3):
 		# blank display
 		disp.fill()
 		disp.show()
@@ -73,6 +77,7 @@ else:
 		scr.draw_image(IMAGES[trialnr])
 		disp.fill(screen=scr)
 		disp.show()
+		libtime.pause(1000)
 		# start recording eye movements
 		tracker.drift_correction()
 		tracker.start_recording()
@@ -85,16 +90,48 @@ else:
 			scr.clear()
 			scr.draw_image(IMAGES[trialnr])
 			gazepos = tracker.sample()
-			scr.draw_circle(pos=gazepos, r=100, fill=True, colour=(127,127,127))
+			scr.draw_circle(pos=gazepos, r=100, fill=True, colour=(127, 127, 127))
 			disp.fill(screen=scr)
 			disp.show()
 			response, presstime = kb.get_key(timeout=1)
 		libtime.pause(1000)
-
+		maskCount = maskCount + 1
 		# stop tracking and process input
 		tracker.stop_recording()
 		tracker.log("stop trial %d" % trialnr)
 		log.write([gazepos, trialnr, trialstart, presstime, presstime-trialstart, IMAGES[trialnr]])
+	elif (trialType == 'full'):
+		# blank display
+		disp.fill()
+		disp.show()
+		libtime.pause(1000)
+		# prepare stimulus
+		scr.clear()
+		scr.draw_image(IMAGES[trialnr])
+		disp.fill(screen=scr)
+		disp.show()
+		libtime.pause(1000)
+		# start recording eye movements
+		tracker.drift_correction()
+		tracker.start_recording()
+		tracker.status_msg("trial %d" % trialnr)
+		tracker.log("start trial %d" % trialnr)
+		# present stimulus
+		response = None
+		trialstart = libtime.get_time()
+		while not response:
+			gazepos = tracker.sample()
+			scr.clear()
+			scr.draw_image(IMAGES[trialnr])
+			disp.fill(screen=scr)
+			disp.show()
+			response, presstime = kb.get_key(timeout=1)
+		libtime.pause(1000)
+		fullCount = fullCount + 1
+		# stop tracking and process input
+		tracker.stop_recording()
+		tracker.log("stop trial %d" % trialnr)
+		log.write([gazepos, trialnr, trialstart, presstime, presstime - trialstart, IMAGES[trialnr]])
 
 # close experiment
 log.close()
